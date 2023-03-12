2d3a51fa0cdc6efab307c61e1b54be45567d22b7
FijolKrzysztof <xkrzysztoffijol@gmail.com>
2023-03-12 16:16:22 +0100

    Poprawki

--- set-up-containers.sh
+++ set-up-containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for name in ['lanstreamer-api' 'lanstreamer-web' 'lanstreamer-auth']; do
+for name in ['lanstreamer-api' 'lanstreamer-web']; do
   docker rm -f "$name"
   docker rmi -f "$name"
   docker load -i "/tmp/$name"
--- upload-containers.py
+++ upload-containers.py
@@ -4,8 +4,7 @@ import command
 
 for container in [
     {'name': 'lanstreamer-api', 'path': '/home/krzysztof/Apps/lanstreamer-api/'},
-    {'name': 'lanstreamer-web', 'path': '/home/krzysztof/Apps/lanstreamer-code/lanstreamer/'},
-    {'name': 'lanstreamer-auth', 'path': '/home/krzysztof/Apps/lanstreamer-code/lanstreamer-web/'},
+    {'name': 'lanstreamer-web', 'path': '/home/krzysztof/Apps/lanstreamer-code/lanstreamer-web/'},
 ]:
     response = command.run(['/home/krzysztof/Apps/devops-scripts/upload-containers.sh', container["name"], container["path"]], debug=True)
     print(response.output)
